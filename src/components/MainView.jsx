72e83c3c4a4c3cd922d6139a8ce98f3aa6eac427
Lucas Mendoza <mendozalucas001@gmail.com>
2025-06-06 15:18:41 -0300

    -

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,3 +1,4 @@
 export const MainView = () => {
-  return <>asd</>;
+  
+  return <></>;
 };
--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -1,11 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const NotesContext = createContext();
 
 export const NotesProvider = ({ children }) => {
-  const [notes, setNotes] = useState([]);
+  const initialState = {
+    notes: [],
+    inputTitle: "",
+    inputText: "",
+  };
+  const reducer = ({ state, action }) => {
+    switch (action.type) {
+      case "ADD_NOTE":
+      case "DELETE_NOTE":
+      case "EDIT_NOTE":
+      case "SELECT_NOTE":
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <NotesContext.Provider value={notes}>{children}</NotesContext.Provider>
+    <NotesContext.Provider value={{ state, dispatch }}>
+      {children}
+    </NotesContext.Provider>
   );
 };
